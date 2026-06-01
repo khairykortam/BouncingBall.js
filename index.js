4293297bccf3ef56e0058fb7fc7200663de3175c
khairykortam <khairykortam270@gmail.com>
2026-06-01 20:37:34 +0300

    intial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+(()=> {
+    const canvas = document.getElementById('canvas');
+    const width = canvas.width;
+    const height = canvas.height;
+    const context = canvas.getContext('2d');
+
+})()
