7e5481d80a6226c13e5f9c541e6b05cee6695b35
khairykortam <khairykortam270@gmail.com>
2026-06-03 19:28:38 +0300

    added a bouncing ball to test

--- index.js
+++
@@ -1,7 +0,0 @@
-(()=> {
-    const canvas = document.getElementById('canvas');
-    const width = canvas.width;
-    const height = canvas.height;
-    const context = canvas.getContext('2d');
-
-})()
